c2908cf466e6f8bf916effe8817614153d91a97b
matslyk0 <matslyk0@gmail.com>
2024-09-12 16:20:10 +0100

    Formatting method is complete. Full ruleset is now in README.

--- src/Helpers.java
+++ src/Helpers.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class Helpers {
 
-    static String[] operationsArray = {"+","-","÷","×","."};
+    static String[] operationsArray = {"+", "-", "÷", "×", "."};
     static List<String> operations = Arrays.asList(operationsArray);
     static DecimalFormat oneNine = new DecimalFormat("#.#########");
     static DecimalFormat twoEight = new DecimalFormat("##.########");
@@ -16,18 +16,28 @@ public class Helpers {
     static DecimalFormat eightTwo = new DecimalFormat("########.##");
     static DecimalFormat nineOne = new DecimalFormat("#########.#");
     static DecimalFormat tenZero = new DecimalFormat("##########");
+    static DecimalFormat scientific = new DecimalFormat("0.#########E0");
     static DecimalFormat[] formats = {
             oneNine, twoEight, threeSeven, fourSix, fiveFive,
-            sixFour, sevenThree, eightTwo, nineOne, tenZero
+            sixFour, sevenThree, eightTwo, nineOne, tenZero,
+            scientific
     };
 
-    static char getLastChar(String text) {return text.charAt(text.length()-1);}
+    static char getLastChar(String text) {
+        return text.charAt(text.length() - 1);
+    }
 
-    static boolean isOperation(char a) {return operations.contains(""+a);}
+    static boolean isOperation(char a) {
+        return operations.contains("" + a);
+    }
 
-    static String cutLastChar(String text) {return text.substring(0,text.length()-1);}
+    static String cutLastChar(String text) {
+        return text.substring(0, text.length() - 1);
+    }
 
-    static String cutFirstChar(String text) {return text.substring(1);}
+    static String cutFirstChar(String text) {
+        return text.substring(1);
+    }
 
     static boolean endsWithOperation(String text) {
         char lastChar = getLastChar(text);
@@ -35,13 +45,22 @@ public class Helpers {
     }
 
     static String formatter(String num) {
-        //the number is rounded to a number of decimal places appropriate to the numbers size
-        //trailing zeros are automatically removed
-        int index = num.indexOf(".")-1;
-        Double numDouble = Double.parseDouble(num);
-        if (index==-2) return num;
-        return formats[index].format(numDouble);
-        //will improve readability
+        Double numAsDouble;
+        try {numAsDouble = Double.parseDouble(num);}
+        catch (NumberFormatException _) {return "";}
+        int leadingDigits = num.indexOf(".");
+
+        if (leadingDigits == -1) {
+            leadingDigits = num.length();
+            if (leadingDigits <= 10) return formats[leadingDigits - 1].format(numAsDouble);
+            else return formats[10].format(numAsDouble);
+        }
+
+        if (num.startsWith("0.") || num.startsWith("0.0")) {
+            if (!num.startsWith("0.00")) return formats[0].format(numAsDouble);
+            else return formats[10].format(numAsDouble);
+        }
+
+        return formats[leadingDigits - 1].format(numAsDouble);
     }
 }
-
